6f6eb041751b58e5f043b055ea4be7b0a4f24471
Neldo Agustin Falcon <psiro.hyarte@gmail.com>
2023-05-23 13:45:27 -0500

    fix: update DB with admin

--- testAnsiedad.sql
+++ testAnsiedad.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12.6 (x86_64)
 --
 -- Host: localhost    Database: testAnsiedad
 -- ------------------------------------------------------
--- Server version	8.0.33-0ubuntu0.22.04.2
+-- Server version	8.0.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -152,7 +152,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'john','','','','jhon@example.com','password1',1,0,0),(2,'mary','','','','mary@example.com','password2',0,0,0),(3,'alex','','','','alex@example.com','password3',1,0,0),(4,'emily','','','','emily@example.com','password4',0,0,0),(5,'david','','','','david@example.com','password5',1,0,0),(6,'eder','','','','eder@example.com','edercito',1,0,0),(12,'abcd',NULL,NULL,NULL,'abcd','abcd',1,0,0),(13,'asdf',NULL,NULL,NULL,'asdf','asdf',0,0,0),(14,'',NULL,NULL,NULL,'','',1,0,0),(15,'eder',NULL,NULL,NULL,'eder','eder',1,0,0),(16,'luis',NULL,NULL,NULL,'luis','luis',1,0,0),(17,'carlos',NULL,NULL,NULL,'carlos','carlos',1,0,0);
+INSERT INTO `user` VALUES (1,'john','','','','jhon@example.com','password1',1,0,0),(2,'mary','','','','mary@example.com','password2',0,0,0),(3,'alex','','','','alex@example.com','password3',1,0,0),(4,'emily','','','','emily@example.com','password4',0,0,0),(5,'david','','','','david@example.com','password5',1,0,0),(6,'eder','','','','eder@example.com','edercito',1,0,0),(12,'abcd',NULL,NULL,NULL,'abcd','abcd',1,0,0),(13,'asdf',NULL,NULL,NULL,'asdf','asdf',0,0,0),(14,'',NULL,NULL,NULL,'','',1,0,0),(15,'admin',NULL,NULL,NULL,'admin','admin',0,0,0),(16,'luis',NULL,NULL,NULL,'luis','luis',1,0,0),(17,'carlos',NULL,NULL,NULL,'carlos','carlos',1,0,0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -165,4 +165,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-23  8:48:20
+-- Dump completed on 2023-05-23 13:44:37
